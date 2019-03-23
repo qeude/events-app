0e2d822d4b9ba3ebc8d75d15f624a0d5fd8f07e2
Quentin Eude <quentineude@gmail.com>
2019-03-23 16:04:09 +0100

    Adding SQLite db + deleting the mock

---
+++ lib/database/database.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:events_app/models/models.dart';
+
+final eventTABLE = 'Event';
+class DatabaseProvider {
+  static final DatabaseProvider dbProvider =  DatabaseProvider();
+
+  Database _database;
+
+  Future<Database> get database async{
+    if (_database != null) return _database;
+    _database = await createDatabase();
+    return _database;
+  }
+
+  createDatabase() async{
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "Event.db");
+    var database = await openDatabase(path,
+        version: 1, onCreate: initDB, onUpgrade: onUpgrade);
+    return database;
+  }
+
+   void onUpgrade(Database database, int oldVersion, int newVersion) {
+    if (newVersion > oldVersion) {}
+  }
+
+  void initDB(Database database, int version) async {
+    await database.execute("CREATE TABLE $eventTABLE ("
+        "id STRING PRIMARY KEY, "
+        "name TEXT,"
+        "description TEXT,"
+        "date TEXT,"
+        "complete INTEGER "
+        ")");
+    await database.insert(eventTABLE, Event('Croatie', DateTime.now()).toDatabaseJson());
+    await database.insert(eventTABLE, Event('Suisse', DateTime.now()).toDatabaseJson());
+    await database.insert(eventTABLE, Event('Italie', DateTime.now()).toDatabaseJson());
+
+
+
+  }
+}
