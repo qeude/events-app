eaa617e1a3c6d84b2484f78d9a92ac870e77c86e
Quentin Eude <quentineude@gmail.com>
2019-03-23 16:04:42 +0100

    Add event repository

---
+++ lib/repository/event_repository.dart
@@ -0,0 +1,18 @@
+import 'package:events_app/dao/event_dao.dart';
+import 'package:events_app/models/models.dart';
+
+class EventRepository{
+  final eventDao = EventDao();
+
+
+  Future getAllEvents({String query}) => eventDao.getEvents(query: query);
+
+  Future insertEvent(Event event) => eventDao.createEvent(event);
+
+  Future updateEvent(Event event) => eventDao.updateEvent(event);
+
+  Future deleteEventByid(String id) => eventDao.deleteEvent(id);
+
+  Future deleteAllEvents() => eventDao.deleteAllEvents();
+  
+}
