517013aa37b3305b6d844657fc1f62da6ba0c5b2
Quentin Eude <quentineude@gmail.com>
2019-03-28 21:28:58 +0100

    Add some constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
 // Text
 const appTitle = 'Events';
 const errorMessage = 'Something went wrong. Try again!';
 const emptyList = 'Event list is empty !';
+const eventNameTextStyle = TextStyle(color: Colors.white, fontSize: 50.0, shadows: [Shadow(color: Colors.black12, blurRadius: 4.0)]);
+const eventDateTextStyle = TextStyle(color: Colors.white, fontSize: 20.0, shadows: [Shadow(color: Colors.black12, blurRadius: 4.0)]);
 
 // Numbers
 const opacityVisible = 1.0;
