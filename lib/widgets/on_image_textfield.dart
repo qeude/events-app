54b6d79e551f56620c9d3e055b9bbff234acfbcd
Quentin Eude <quentineude@gmail.com>
2019-04-16 13:03:48 +0200

    Adding controller to On Image Textfield

--- lib/widgets/on_image_textfield.dart
+++ lib/widgets/on_image_textfield.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 class OnImageTextField extends StatelessWidget {
   final TextStyle style;
   final String hintText;
-
-  OnImageTextField(this.style, this.hintText);
+  final TextEditingController controller;
+  OnImageTextField(this.style, this.hintText,{this.controller});
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: Colors.white,
+      controller: controller,
       style: this.style,
       maxLines: null,
       maxLength: 50,
