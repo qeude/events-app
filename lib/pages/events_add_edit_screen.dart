d3168c8465a77a0d6950e1bcaf6f3c8466738885
Quentin Eude <quentineude@gmail.com>
2019-05-09 22:09:28 +0200

    Fix hero animation

--- lib/pages/events_add_edit_screen.dart
+++ lib/pages/events_add_edit_screen.dart
@@ -10,8 +10,6 @@ import 'package:events_app/utils/constants.dart';
 import 'package:events_app/utils/utils.dart';
 import 'package:events_app/models/models.dart';
 import 'package:events_app/blocs/add_event/add_event_bloc.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
 
 class EventsAddEditScreen extends StatefulWidget {
   final String id;
@@ -23,13 +21,9 @@ class EventsAddEditScreen extends StatefulWidget {
 
 class _EventsAddEditScreenState extends State<EventsAddEditScreen> {
   bool hasDescription = false;
-
   bool hasLocation = false;
-
   bool hasChecklist = false;
-
   DateTime eventDate;
-
   TextEditingController eventNameController = new TextEditingController();
   TextEditingController descriptionController = new TextEditingController();
   final AddEventBloc addEventBloc = AddEventBloc();
@@ -118,7 +112,7 @@ class _EventsAddEditScreenState extends State<EventsAddEditScreen> {
                               ? <Widget>[
                                   IconButton(
                                     icon: Icon(Icons.image),
-                                    onPressed: () => {getImage()},
+                                    onPressed: () async => {await getImage()},
                                   ),
                                   IconButton(
                                     icon: Icon(Icons.check),
@@ -271,8 +265,8 @@ class _EventsAddEditScreenState extends State<EventsAddEditScreen> {
               ),
             ],
           ))),
-      onTap: () {
-        getImage();
+      onTap: () async {
+        await getImage();
       },
     );
   }
@@ -378,39 +372,7 @@ class _EventsAddEditScreenState extends State<EventsAddEditScreen> {
                   style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
                 )),
           ),
-          Container(
-              height: 150,
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: FlutterMap(
-                    options: MapOptions(
-                      interactive: false,
-                      center: LatLng(48.8534, 2.3488),
-                      minZoom: 13.0,
-                      maxZoom: 13.0,
-                      zoom: 13.0,
-                    ),
-                    layers: [
-                      TileLayerOptions(
-                          urlTemplate:
-                              'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                          subdomains: ['a', 'b', 'c']),
-                      MarkerLayerOptions(markers: <Marker>[
-                        Marker(
-                          width: 50.0,
-                          height: 50.0,
-                          point: LatLng(48.8534, 2.3488),
-                          builder: (ctx) => Container(
-                                child: Icon(
-                                  Icons.location_on,
-                                  color: Colors.red,
-                                  size: 50.0,
-                                ),
-                              ),
-                        )
-                      ])
-                    ],
-                  ))),
+          buildEventLocation()
         ],
       ),
     );
--- lib/pages/events_details_screen.dart
+++ lib/pages/events_details_screen.dart
@@ -7,12 +7,17 @@ import 'package:events_app/models/models.dart';
 import 'package:events_app/utils/constants.dart';
 import 'package:events_app/utils/utils.dart';
 import 'package:events_app/widgets/loading_indicator.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
+
 class EventsDetailsScreen extends StatefulWidget {
   final String id;
-
-  EventsDetailsScreen({@required this.id});
+  final String image;
+  final String name;
+  final DateTime date;
+  EventsDetailsScreen(
+      {@required this.id,
+      @required this.image,
+      @required this.name,
+      @required this.date});
 
   @override
   _EventsDetailsScreenState createState() => _EventsDetailsScreenState();
@@ -34,85 +39,91 @@ class _EventsDetailsScreenState extends State<EventsDetailsScreen> {
       stream: eventBloc.eventDetails,
       builder: (context, snapshot) {
         Event event;
-        if (snapshot.hasData) {
-          event = snapshot.data;
-          return Stack(
-            children: <Widget>[
-              Column(children: <Widget>[
-                Expanded(
-                    child: ListView(
-                  shrinkWrap: true,
-                  children: <Widget>[
-                    _buildEventImage(context, event),
-                    (event.description != null &&
-                            event.description.trim().isNotEmpty)
-                        ? _buildDescriptionSection(context, event)
-                        : Container(),
-                    event.hasLocation ? _buildLocationSection(context) : Container()
-                  ],
-                ))
-              ]),
-              new Positioned(
-                //Place it at the top, and not use the entire screen
-                top: 0.0,
-                left: 0.0,
-                right: 0.0,
-                child: AppBar(
-                  actions: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.delete_forever),
-                      onPressed: () async {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context){
-                            return AlertDialog(
-                              title: Text('Delete ${event.name}'),
-                              content: Text('You will not be able to get it back, are you sure that you want delete this event ?'),
-                              actions: <Widget>[
-                                 FlatButton(
-                                  child: Text("Cancel"),
-                                  onPressed: (){
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                                FlatButton(
-                                  child: Text('Delete'),
-                                  onPressed: (){
-                                    eventBloc.deleteEvent(event);
-                                    Navigator.of(context).popUntil(ModalRoute.withName("/"));
-                                  },
-                                ),
-                              ],
-                            );
-                        });
-                      },),
-                    IconButton(
-                        icon: Icon(Icons.create),
-                        onPressed: () async => await Navigator.of(context)
-                                .push(MaterialPageRoute(builder: (_) {
-                              return EventsAddEditScreen(id: widget.id);
-                            })))
-                  ],
-                  backgroundColor: Colors.transparent, //No more green
-                  elevation: 0.0, //Shadow gone
-                ),
+        if (snapshot.hasData) event = snapshot.data;
+        return Stack(
+          children: <Widget>[
+            Column(children: <Widget>[
+              Expanded(
+                  child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  _buildEventImage(context, event),
+                  (event != null &&
+                          event.description != null &&
+                          event.description.trim().isNotEmpty)
+                      ? _buildDescriptionSection(context, event)
+                      : Container(),
+                  (event != null && event.hasLocation)
+                      ? _buildLocationSection(context)
+                      : Container()
+                ],
+              ))
+            ]),
+            new Positioned(
+              top: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: AppBar(
+                actions: _buildActionButtons(context, event),
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
               ),
-            ],
-          );
-        }
-        return Container(child: LoadingIndicatorWidget(visible: true));
+            ),
+          ],
+        );
       },
     ));
   }
 
+  List<Widget> _buildActionButtons(BuildContext context, Event event) {
+    return [
+      IconButton(
+        icon: Icon(Icons.delete_forever),
+        onPressed: () async {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Delete ${event.name}'),
+                  content: Text(
+                      'You will not be able to get it back, are you sure that you want delete this event ?'),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("Cancel"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    FlatButton(
+                      child: Text('Delete'),
+                      onPressed: () {
+                        eventBloc.deleteEvent(event);
+                        Navigator.of(context)
+                            .popUntil(ModalRoute.withName("/"));
+                      },
+                    ),
+                  ],
+                );
+              });
+        },
+      ),
+      IconButton(
+          icon: Icon(Icons.create),
+          onPressed: () async =>
+              await Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                return EventsAddEditScreen(id: widget.id);
+              })))
+    ];
+  }
+
   Widget _buildEventImage(BuildContext context, Event event) {
-    //TODO: Fix Hero animation because of state
     return Hero(
-      tag: 'eventImg${event.id}',
+      tag: 'eventImg${event != null ? event.id : widget.id}',
       child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-                image: eventImageProvider(event.image),
+                image: eventImageProvider(
+                    event != null ? event.image : widget.image),
                 alignment: Alignment.topCenter,
                 fit: BoxFit.cover),
           ),
@@ -127,14 +138,15 @@ class _EventsDetailsScreenState extends State<EventsDetailsScreen> {
                 child: Material(
                     color: Colors.transparent,
                     child: Text(
-                      event.name,
+                      event != null ? event.name : widget.name,
                       style: eventNameTextStyle,
                     )),
               ),
               Container(
                   child: Material(
                       color: Colors.transparent,
-                      child: Text("In ${getTimeUntilEvent(event.date)}",
+                      child: Text(
+                          "In ${event != null ? getTimeUntilEvent(event.date) : getTimeUntilEvent(widget.date)}",
                           style: eventDateTextStyle)))
             ],
           )),
@@ -163,6 +175,7 @@ class _EventsDetailsScreenState extends State<EventsDetailsScreen> {
       ),
     );
   }
+
   _buildLocationSection(BuildContext context) {
     return Container(
       alignment: Alignment.topLeft,
@@ -179,39 +192,7 @@ class _EventsDetailsScreenState extends State<EventsDetailsScreen> {
                   style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
                 )),
           ),
-          Container(
-              height: 150,
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: FlutterMap(
-                    options: MapOptions(
-                      interactive: false,
-                      center: LatLng(48.8534, 2.3488),
-                      minZoom: 13.0,
-                      maxZoom: 13.0,
-                      zoom: 13.0,
-                    ),
-                    layers: [
-                      TileLayerOptions(
-                          urlTemplate:
-                              'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                          subdomains: ['a', 'b', 'c']),
-                      MarkerLayerOptions(markers: <Marker>[
-                        Marker(
-                          width: 50.0,
-                          height: 50.0,
-                          point: LatLng(48.8534, 2.3488),
-                          builder: (ctx) => Container(
-                                child: Icon(
-                                  Icons.location_on,
-                                  color: Colors.red,
-                                  size: 50.0,
-                                ),
-                              ),
-                        )
-                      ])
-                    ],
-                  ))),
+          buildEventLocation(),
         ],
       ),
     );
